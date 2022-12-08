fa411f26571d105a49f41cf961c65e6b430adf85
rybas <bibiano.rivas.garcia@gmail.com>
2022-12-08 16:30:04 +0100

    first lessons

---
+++ src/cleanCodeNaming/03-functions.ts
@@ -0,0 +1,72 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBioById( id: string ) {
+        console.log({ id });
+    }
+    
+    // Crear una película
+    function movie(title: string, description: string, rating: number, cast: string[] ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    interface Movie {
+        title: string;
+        description: string;
+        rating: number;
+        cast: string [];
+    }
+    // Crear una película
+    function createMovie({ title, description, rating, cast }: Movie) {
+
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result : number;
+
+        if ( isDead ) {
+            return 1500;
+        }
+        
+        if ( isSeparated ) {
+            return 2500;
+        }
+        
+        return (isRetired) ? 3000 : 4000;
+        
+        
+       
+    }
+    
+
+
+})();
+
+
+
+
---
+++ src/cleanCodeNaming/getPayAmount.ts
@@ -0,0 +1,39 @@
+const getPayAmountOriginal = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+    let result;
+    if ( isDead ) {
+        result = 1500;
+    } else {
+        if ( isSeparated ) {
+            result = 2500;
+        } else {
+            if ( isRetired ) {
+                result = 3000;
+            } else {
+                result = 4000; 
+            }
+        }
+    }
+    
+    return result;
+}
+
+
+const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+    let result;
+    if ( isDead ) {
+        result = 1500;
+    } else {
+        if ( isSeparated ) {
+            result = 2500;
+        } else {
+            if ( isRetired ) {
+                result = 3000;
+            } else {
+                result = 4000; 
+            }
+        }
+    }
+    
+    return result;
+}
+
