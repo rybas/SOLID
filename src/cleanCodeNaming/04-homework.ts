2e7d3f805538fced899386acf552bfe0bca673ee
rybas <bibiano.rivas.garcia@gmail.com>
2022-12-08 17:43:36 +0100

    lesson 4

--- src/cleanCodeNaming/04-homework.ts
+++ src/cleanCodeNaming/04-homework.ts
@@ -3,20 +3,38 @@
 
     // Resolver sin la triple condicional dentro del if
     // includes? arrays?
-    function isRedFruit( fruit: string ): boolean {
+    function isRedFruit2( fruit: string ): boolean {
         
-        if ( fruit === 'manzana' || fruit === 'cereza' || fruit === 'ciruela' ) {
+        /* if ( fruit === 'manzana' || fruit === 'cereza' || fruit === 'ciruela' ) {
             return true;
         } else {
             return false;
-        }
+        }*/
+
+        /*REFACTORIZADO*/
+        const fruits = [{element:'manzana'}, {element:'cereza'}, {element: 'ciruela'}];
+
+        return fruits.some(f => f.element === fruit);
+        
+       
+    }
+
+    function isRedFruit( fruit: string ): boolean {
+        
+        /*REFACTORIZADO curso*/
+
+        const redFruits = ['manzana','cereza','ciruela'];
+
+        return redFruits.includes(fruit);
+        //return ['manzana','cereza','ciruela'].includes(fruit);
+
     }
 
     // Simplificar esta función
     // switch? Object literal? validar posibles colores
-    function getFruitsByColor( color: string ): string[] {
+    function getFruitsByColor2( color: string ): string[] {
 
-        if ( color === 'red' ) {
+        /*if ( color === 'red' ) {
             return ['manzana','fresa'];
         } else if ( color === 'yellow') {
             return ['piña','banana'];
@@ -24,8 +42,68 @@
             return ['moras','uvas']
         } else {
             throw Error('the color must be: red, yellow, purple');
+        }*/
+
+        /**REFACTORIZADO yo*/
+
+        /**
+         * const map1 = new Map<number, string[]>([
+                [0, ['a', 'b']],
+                [1, ['c', 'd']],
+                ]);
+
+                map1.set(2, ['e', 'f']);
+
+                // 👇️ ['e', 'f']
+                console.log(map1.get(2));
+         */
+        let colours: string[] =[];
+        const colorFruits: {[name: string]:string []} = {
+            'red' : ['manzana','fresa'],
+            'yellow': ['piña','banana'],
+            'purple' : ['moras','uvas']
+        };
+            
+        const map = new Map<string, string[]>(Object.entries(colorFruits));
+        console.log(map.get(color));
+        if (map.get(color)){
+            colours = map.get(color)??[];
+        }else{
+            throw Error('the color must be: red, yellow, purple');
         }
+        
+        return colours;
     }
+
+    type FruitColor = 'red'|'yellow'|'purple';
+
+    function getFruitsByColor( color: string ): string[] {
+
+        /**REFACTORIZADO curso*/
+        const fruitsByColor = {
+            red: ['manzana','fresa'],
+            yellow: ['piña','banana'],
+            purple: ['moras','uvas']
+        };
+
+        if (!Object.keys(fruitsByColor).includes(color)) {
+            throw Error('the color must be: red, yellow, purple');
+        }
+            
+        return fruitsByColor[color];
+       
+        /*switch(color){
+            case 'red':
+                return ['manzana','fresa'];
+            case 'yellow':
+                return ['piña','banana'];
+            case 'purple':
+                return ['moras','uvas'];
+            default:
+                throw Error('the color must be: red, yellow, purple');
+        }*/
+    }
+
 
     // Simplificar esta función
     let isFirstStepWorking  = true;
@@ -34,7 +112,7 @@
     let isFourthStepWorking = true;
 
     function workingSteps() {
-        if( isFirstStepWorking === true ) {
+       /* if( isFirstStepWorking === true ) {
             if( isSecondStepWorking === true ) {
                 if( isThirdStepWorking === true ) {
                     if( isFourthStepWorking === true ) {
@@ -55,6 +133,13 @@
         else {
             return 'First step broken.';
         }
+    */
+        if ( !isFirstStepWorking === true )  return 'First step broken.';
+        if ( !isSecondStepWorking === true ) return 'Second step broken.'; 
+        if ( !isThirdStepWorking === true )  return 'Third step broken.';
+        if ( !isFourthStepWorking === true )  return 'Fourth step broken.';
+
+        return 'Working properly!';
     }
 
 
